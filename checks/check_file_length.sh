7a81f1f36c226f27cfe5d852bf4922b1e2541253
DanielSuchy <daniel.suchy@gmail.com>
2020-06-15 11:31:29 +0200

    check same number of lines for devset

--- checks/check_file_length.sh
+++ checks/check_file_length.sh
@@ -3,16 +3,54 @@
 #verifies that paralel text files (OST, TTcs{1,2}, TTde) have the same number of lines.
 
 cd "${0%/*}/.."
+root_dir='./documents/iwslt2020-nonnative-slt/devset'
 
-set -e
+for ost in `ls $root_dir/*.OSt`; do #everything has an OSt
+  if test -f "$ost"; then
+    length_ost=`cat $ost | wc -l`
+  fi
 
-l1=`cat ./documents/iwslt2020-nonnative-slt/devset/ami.IS1001a.OSt | wc -l`
-l2=`cat ./documents/iwslt2020-nonnative-slt/devset/ami.IS1001a.TTcs | wc -l`
+  ttcs=`echo $ost | sed 's/OSt/TTcs/g'`
+  if test -f "$ttcs"; then
+    length_ttcs=`cat $ttcs | wc -l`
+  fi
 
+  ttcs1=`echo $ost | sed 's/OSt/TTcs1/g'`
+  if test -f "$ttcs1"; then
+    length_ttcs1=`cat $ttcs1 | wc -l`
+  fi
 
-if [ $l1 -eq $l2 ]; then
-  exit 0
-else
-  echo "files have incorrect number of lines"
-  exit 1
-fi
+  ttcs2=`echo $ost | sed 's/OSt/TTcs2/g'`
+  if test -f "$ttcs2"; then
+    length_ttcs2=`cat $ttcs2 | wc -l`
+  fi
+
+  ttde=`echo $ost | sed 's/OSt/TTde/g'`
+  if test -f "$ttde"; then
+    length_ttde=`cat $ttde | wc -l`
+  fi
+
+  if test -f "$ttcs"; then
+    are_lengths_equal=`echo -e "${length_ost}\n${length_ttcs}\n${length_ttde}" | sort -u | wc -l`
+    if [ $are_lengths_equal -ne 1 ]; then
+      echo "$ost and its paralel documents don't have the same number of lines"
+      echo "$ost : $length_ost"
+      echo "$ttcs : $length_ttcs"
+      echo "$ttde : $length_ttde"
+      exit 1 #not equal is an error
+    fi
+  else
+    are_lengths_equal=`echo -e "${length_ost}\n${length_ttcs1}\n${length_ttcs2}\n${length_ttde}" | sort -u | wc -l`
+    if [ $are_lengths_equal -ne 1 ]; then
+      echo "$ost and its paralel documents don't have the same number of lines"
+      echo "$ost : $length_ost"
+      echo "$ttcs1 : $length_ttcs1"
+      echo "$ttcs2 : $length_ttcs2"
+      echo "$ttde : $length_ttde"
+      exit 1 #not equal is an error
+    fi
+  fi
+
+done
+
+exit 0
