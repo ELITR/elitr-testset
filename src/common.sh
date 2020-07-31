de8329c09ee4b95a45dd3d37ea8eee9ee72e8dc1
Sagar Sangeet <sagar@ufal.mff.cuni.cz>
2020-07-31 17:15:53 +0200

    updating the scripts

--- src/common.sh
+++ src/common.sh
@@ -15,15 +15,7 @@ TCLIENT_PORT=4448    #   UEDIN Textclient Port
 PUBTCLIENT_PORT=4448 #   Pervoice publisher client port
 
 ## set following before running common:
-# Separate path to save logs-
-bname=$(basename $1)
-LOGSPATH=$(git rev-parse --show-toplevel)/logs/$(dirname "$1" | cut -d"/" -f10-)
 
-
-DATE=$(date '+%Y%m%d-%H%M%S')
-filename=$(basename "$bname" | cut -d. -f1,2)
-DIR=${LOGSPATH}/${filename}.pvlogs
-TRDIR=${LOGSPATH}/${filename}.transcript
 
 web_link="ws://quest.ms.mff.cuni.cz:5000/textflow"
 
@@ -48,32 +40,18 @@ add_params="--size=100 --min_status 1 10 --mask-k 1 --finalization-time 300 --vo
 
 [ ! -f ./textclient ] && ln -rs $TCLIENT ./textclient
 TCLIENT="$PPATH/mt-cache.py ./textclient -s $MEDIATOR -p $TCLIENT_PORT" # --timestampsIn --timestampsOut ommited by Dominik
-PUBTCLIENT="./textclient -s $MEDIATOR -p $PUBTCLIENT_PORT"
 
 SEGM_TCLIENT="./textclient -s $MEDIATOR -p $TCLIENT_PORT "
 # Integrated with MT wrapper
 NEW_TCLIENT="$mt_wrapper --mt "\""./textclient -p $TCLIENT_PORT "
-#SEGM_TCLIENT="$segm_wrapper --mt "\"" $UNB ./textclient -p $TCLIENT_PORT "
-SUBTITLER_MT=$(git rev-parse --show-toplevel)"/chopper/subtitler.sh --width=70 --desired-flicker=3"
 
-# As per Ondrej's request- too assess the flicker in the subtitles
-SUBTITLER_MT_0=$(git rev-parse --show-toplevel)"/chopper/subtitler.pl --width=70 --desired-flicker=3"
-SUBTITLER_MT_1=$(git rev-parse --show-toplevel)"/chopper/subtitler.pl --width=70 --desired-flicker=2"
-SUBTITLER_MT_2=$(git rev-parse --show-toplevel)"/chopper/subtitler.pl --width=70 --desired-flicker=1"
 czech_asr=$(git rev-parse --show-toplevel)/cuni-kaldi-czech-asr
 
-if [ -z "$DIR" ]; then
-    DIR=.
-else
-    mkdir -p $DIR
-    mkdir -p $TRDIR
-fi
 
-cs_cmd="stdbuf -o0 nc localhost $PORT | $UNB $czech_asr/obsolete/timestamps-to-januslike.pl | $UNB python3 $PPATH/lower.py | $UNB sed 's/_sil_/ /g' | $UNB egrep '[0-9]+ [0-9]+ \S'"
-jasper_cmd="stdbuf -o0 nc localhost $PORT "
 
 otf="online-text-flow-"
 # # for worker UEDIN's rb-EU_fromEN-en_to_41
 rb_langs=("de" "ro" "cs" "fr" "hu" "pl" "nl")
 split_list=('2' '4'  '6'  '8' '10' '12' '14')
+
 
--- src/text2text.sh
+++ src/text2text.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 
-input="test.MT"
-#line_mt=$(stdbuf -oL ./textclient -p 4448 -f en-EU -i rb-EU_fromEN-en_to_41 < $input)
-#$echo $line_mt
-#exit
-IFS=''
-while read line; do
+if [ $# -ne 4 ] ; then
+    echo "usage: $0 <ip-fingerprint> <op-fingerprint> <input dir with logs> <dir containing reference transcripts>"
+    exit
+fi
 
-  num_fields=$(echo $line |  awk -F"\t" '{print NF}')
-  num_langs=$((num_fields / 2))
+IP_FP=$1 # eg. en-EU
+OP_FP=$2 # eg. rb-EU_fromEN-en_to_41
+pvlogs_path=$3 # input dir with logs eg. elitr-testset/logs/iwslt2020-nonnative-slt/testset/antrecorp/en-EU-ufal-da-20200120/03_botel-proti-proudu.OS_16K.pvlogs
+REF_DIR=$4 # Path containing the reference transcripts
 
- for ((i=1; i<=num_langs; i++)); do
-    lang=$(echo $line | cut -d$'\t' -f$(((2 * i) - 1)))
-    text=$(echo $line | cut -d$'\t' -f$((2 * i)))
+TRDIR=$(echo $pvlogs_path | sed 's/pvlogs/transcript/g' )
+MT_TR_DIR=${TRDIR}/${OP_FP} # Transcript dir for MT
 
-#    echo $lang
-#    echo $text
-    echo $text >> tmp/${lang}-MT
+filename=$(basename $pvlogs_path | rev | cut -d. -f2- | rev)
+input=$(find ${TRDIR} -name "${filename}.ASR*") # Get the fullpath of the ASR trascript file
+
+output_file=${pvlogs_path}/${OP_FP}/${filename}.MTrb
+
+mkdir -p ${MT_TR_DIR} ${pvlogs_path}
+
+
+stdbuf -oL ./textclient -p 4448 -f ${IP_FP} -i ${OP_FP} < $input > $output_file
+
+# Generate readable transcripts for the above translations made
+if [[ $OP_FP == *"rb-EU"* ]]; then
+  ## Works only for rainbow workers
+  IFS=''
+  while read line; do
+    num_fields=$(echo $line |  awk -F"\t" '{print NF}')
+    num_langs=$((num_fields / 2))
+
+    for ((i=1; i<=num_langs; i++)); do
+      lang=$(echo $line | cut -d$'\t' -f$(((2 * i) - 1)))
+      text=$(echo $line | cut -d$'\t' -f$((2 * i)))
+
+  #    echo $lang
+  #    echo $text
+      echo $text >> ${MT_TR_DIR}/${filename}.tempMT${lang}
+    done
+  done < "$output_file"
+else
+
+  lang=${OP_FP:0:2}
+  cp -- ${output_file} ${MT_TR_DIR}/${filename}.tempMT${lang}
+fi
+
+# Re-alignment of MT trasncript generated above using reference translations
+for i in ${MT_TR_DIR}/*MT*; do
+  filename=$(basename "$i" | rev | cut -d. -f2- | rev)
+  lang=$( echo $i | rev | cut -d"." -f1 | rev | sed 's/MT//g' | sed 's/temp//g' )
+  reference=$(echo $filename | sed 's/.OS_16K//g' ).TT${lang}
+  
+  # This loop would search for the above filename's reference translation
+  for item in $REF_DIR/*"${reference}"*; do
+      echo $item
+      echo $i
+      ./segment.sh $item ${lang} < $i > ${MT_TR_DIR}/${filename}.MT${lang}
   done
-done < "$input"
+done
