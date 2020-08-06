2749b4a7a58e2fb29412ddf1bcb54e0773ac3e7e
DanielSuchy <daniel.suchy@gmail.com>
2020-08-06 14:18:55 +0200

    evaluate against multiple references

--- src/align.sh
+++ src/align.sh
@@ -3,7 +3,7 @@
 mt=$1
 ostt=$2
 tt=$3
-path=$mt
+path=$4
 
 #SLTev requires files to be in certain directories, so we need to cp and cd
 cp $tt ./src/SLTev/giza-pp/run_giza/tt
@@ -12,7 +12,7 @@ python3 giza++/transcript_to_source.py ../../$ostt > ./giza-pp/run_giza/source_r
 
 cd giza-pp/run_giza
 bash run.sh tt source_ref ./out_folder
-cat out_folder/Result.A3.final > ../../../../$mt.align
+cat out_folder/Result.A3.final > ../../../../$path.align
 
 #delete temporary files
 rm -rf ./out_folder/*
--- src/evaluate_single.sh
+++ src/evaluate_single.sh
@@ -3,7 +3,8 @@
 mt=$1
 ostt=$2
 tt=$3
-ref_align="$mt.align" #a file always in the same directory as mt, with additional extension
+output=$4
+ref_align="$output.align" #a file always in the same directory as mt, with additional extension
 
 cd ./src/SLTev
-python3 SLTev.py --ostt ../../$ostt --tt ../../$tt --mt ../../$mt --align ../../$ref_align --b_time 3000 > ../../"$mt.eval"
+python3 SLTev.py --ostt ../../$ostt --tt ../../$tt --mt ../../$mt --align ../../$ref_align --b_time 3000 > ../../"$output.eval"
