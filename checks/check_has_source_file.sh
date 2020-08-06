b10ca45c1133722a09a3d81064ffa82407cbcfca
DanielSuchy <daniel.suchy@gmail.com>
2020-08-06 14:18:55 +0200

    evaluate against multiple references

--- checks/check_has_source_file.sh
+++ checks/check_has_source_file.sh
@@ -2,8 +2,9 @@
 
 cd "${0%/*}/.."
 exit_code=0
+root_dir="./documents"
 
-for file in `find . -name "*.TT*" -not -path "*output*"`; do
+for file in `find "$root_dir" -name "*.TT*" -not -path "*output*"`; do
 	ost=`echo $file | sed 's/TT.*/OSt/g'`
 	missing=$(find $ost 2>&1)
 	if [[ $missing == *"No such file"* ]]; then
--- src/evaluate.sh
+++ src/evaluate.sh
@@ -10,8 +10,14 @@ for mt in `find $root_dir -type f -name *"MTcs"` ; do
 	name=`echo $name | cut -d "." -f 1`
 
 	ostt=`find "$reference_dir" -name "*$name*.OStt" -not -path *"outputs"*`
-	translation=`find "$reference_dir" -name "*$name*.TTcs1" -not -path *"outputs"*`
+	translation=`find "$reference_dir" -name "*$name*.TTcs*" -not -path *"outputs"*`
 
-	./src/align.sh $mt $ostt $translation
-	./src/evaluate_single.sh $mt $ostt $translation
+	for tt in $translation; do #some machine translations can have multiple reference files
+		extension=`echo $tt | awk 'BEGIN{ FS = "."} { print $NF }'`
+		result_name=`echo "$mt.$extension"` #evaluation and alignment files for each of the translations
+		./src/align.sh $mt $ostt $tt $result_name
+		./src/evaluate_single.sh $mt $ostt $tt $result_name
+	done	
+
+
 done
