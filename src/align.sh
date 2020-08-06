705e3dd10c6c2b3df95dc5366b7af07c736c20e7
DanielSuchy <daniel.suchy@gmail.com>
2020-08-06 09:51:08 +0200

    simplify and comment scripts

--- src/align.sh
+++ src/align.sh
@@ -3,13 +3,18 @@
 mt=$1
 ostt=$2
 tt=$3
-path=$4
+path=$mt
 
+#SLTev requires files to be in certain directories, so we need to cp and cd
+cp $tt ./src/SLTev/giza-pp/run_giza/tt
 cd ./src/SLTev
-python3 giza++/transcript_to_source.py ../../$ostt > source_ref
-cd giza-pp/run_giza
+python3 giza++/transcript_to_source.py ../../$ostt > ./giza-pp/run_giza/source_ref
 
-mv ../../source_ref .
-cp ../../../../$tt tt
+cd giza-pp/run_giza
 bash run.sh tt source_ref ./out_folder
 cat out_folder/Result.A3.final > ../../../../$mt.align
+
+#delete temporary files
+rm -rf ./out_folder/*
+rm ./*source_ref*
+rm ./*tt*
