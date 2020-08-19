217f5ccefdf9581dd83a0364519116d9091b7feb
DanielSuchy <daniel.suchy@gmail.com>
2020-08-19 17:26:12 +0200

    index iwslt-devset

--- indices/scripts/iwslt-devset.sh
+++ indices/scripts/iwslt-devset.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-rm ./iwslt-devset
-root_dir='../documents/iwslt2020-nonnative-slt/devset'
+root_dir='../documents/iwslt2020-nonnative-slt/devset/'
 
-for ost in `ls $root_dir/*.OSt`; do #everything has an OSt
-  echo $ost >> ./iwslt-devset
+for file in `find "$(cd $root_dir; pwd)" -type f -not -name "README.md" | sort`; do
+	group=`echo $file | awk 'BEGIN { FS = "/" } { print $NF }' | cut -d"." -f 1`
 
-  mp3=`echo $ost | sed 's/OSt/mp3/g'`
-  ls $mp3 2>/dev/null >> ./iwslt-devset
-
-  os_mp3=`echo $ost | sed 's/OSt/OS.mp3/g'`
-  ls $os_mp3 2>/dev/null >> ./iwslt-devset
-
-  ttcs=`echo $ost | sed 's/OSt/TTcs/g'`
-  ls $ttcs 2>/dev/null >> ./iwslt-devset
-
-  ttcs1=`echo $ost | sed 's/OSt/TTcs1/g'`
-  ls $ttcs1 2>/dev/null >> ./iwslt-devset
-
-  ttcs2=`echo $ost | sed 's/OSt/TTcs2/g'`
-  ls $ttcs2 2>/dev/null >> ./iwslt-devset
-
-  ttde=`echo $ost | sed 's/OSt/TTde/g'`
-  ls $ttde 2>/dev/null >> ./iwslt-devset
-
-  # ../path_to_file/output_filename.json
-  name=`echo "${ost}" | cut -d"/" -f 5` #name of the file
-  path=`echo "${ost}" | cut -d"/" -f 1-4` #path to the file
-  output=`echo "${path}/output_${name}" | sed 's/OSt/json/g'`
-  ls $output 2>/dev/null >> ./iwslt-devset
-
-  echo "" >> ./iwslt-devset
-done
+	if [[ "$previous_group" != "$group" ]]; then
+		echo -e "\n"
+	fi
+	echo "$file"
+	previous_group=$group
+done 
