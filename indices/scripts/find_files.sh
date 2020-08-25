5907b48fdf50feb9cb5015a56334ba637a935ce2
DanielSuchy <daniel.suchy@gmail.com>
2020-08-25 14:41:11 +0200

    use relative paths for indexes

--- indices/scripts/find_files.sh
+++ indices/scripts/find_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#list all test set files in a directory, gourping them by their names
+#list all test set files in a directory, grouping them by their names
 
 root_dir=$1
 
@@ -8,12 +8,12 @@ if [[ $root_dir == "" ]]; then
 	exit
 fi
 
-for file in `find "$(cd $root_dir; pwd)" -type f -not -name "README.md" | sort`; do
+for file in `find "$root_dir" -type f -not -name "README.md" | sort`; do
 	group=`echo $file | awk 'BEGIN { FS = "/" } { print $NF }' | cut -d"." -f 1`
 
 	if [[ "$previous_group" != "$group" ]]; then
 		echo -e "\n"
 	fi
-	echo "$file"
+	echo "$file" | sed 's:../../::g'
 	previous_group=$group
 done 
--- indices/scripts/iwslt-wgvat.sh
+++ indices/scripts/iwslt-wgvat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "${0%/*}/.."
-./scripts/find_files.sh /home/account/work/elitr-testset/documents/iwslt2020-nonnative-slt/testset/sao-wgvat > iwslt-wgvat
+./scripts/find_files.sh ../../elitr-testset/documents/iwslt2020-nonnative-slt/testset/sao-wgvat > iwslt-wgvat
