5b4a1e9916fc3e85d9d1c670ef5ce7ed843df7a6
DanielSuchy <daniel.suchy@gmail.com>
2020-08-25 17:26:16 +0200

    index asr-czech-any-domain

---
+++ indices/scripts/run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#run all index scripts, to create lists based on the current state of the repo
+
+cd "${0%/*}/.."
+#these are simple indexes based on directories, they need to run first
+./scripts/czech-asr.sh
+./scripts/elitr-testsuite.sh
+./scripts/exotic-languages.sh
+./scripts/iwslt-antrecorp.sh
+./scripts/iwslt-consecutive.sh
+./scripts/iwslt-devset.sh
+./scripts/iwslt-khanacademy.sh
+./scripts/iwslt-wgvat.sh
+./scripts/langtools-workshop.sh
+./scripts/linguistic-mondays.sh
+./scripts/taus.sh
+
+#these are indexes grouped by language, domain, data-types, etc. They extract from simple indexes
+./scripts/asr-any-language-any-domain.sh
+./scripts/asr-czech-any-domain.sh
