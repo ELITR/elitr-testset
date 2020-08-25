ff853b91572740a40566a489ec26e35e6d67790f
DanielSuchy <daniel.suchy@gmail.com>
2020-08-25 14:41:11 +0200

    use relative paths for indexes

--- indices/scripts/exotic-languages.sh
+++ indices/scripts/exotic-languages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "${0%/*}/.."
-./scripts/find_files.sh /home/account/work/elitr-testset/documents/exotic-languages > exotic-languages
+./scripts/find_files.sh ../../elitr-testset/documents/exotic-languages > exotic-languages
