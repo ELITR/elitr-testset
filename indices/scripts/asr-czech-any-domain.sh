94e96093f54c56deae06155a4ab97a64bd3cc918
DanielSuchy <daniel.suchy@gmail.com>
2020-12-16 13:21:18 +0100

    rename automatically created indeces to auto-*

--- indices/scripts/asr-czech-any-domain.sh
+++ indices/scripts/asr-czech-any-domain.sh
@@ -2,8 +2,8 @@
 #list all data that can be used to evaluate czech asr (that have a recording and an OSt)
 
 cd "${0%/*}/.."
-f1=`cat ./czech-asr | grep 'OSt\|mp3\|OStt'`
-f2=`cat ./iwslt-wgvat| grep 'IS'`
+f1=`cat ./auto-czech-asr | grep 'OSt\|mp3\|OStt'`
+f2=`cat ./auto-iwslt2020-wgvat| grep 'IS'`
 
 files=`echo "$f1 $f2"`
-./scripts/group_files.sh "$files" > asr-czech-any-domain
+./scripts/group_files.sh "$files" > auto-asr-czech-any-domain
--- indices/scripts/iwslt-khanacademy.sh
+++ indices/scripts/iwslt-khanacademy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "${0%/*}/.."
-./scripts/find_files.sh ../../elitr-testset/documents/iwslt2020-nonnative-slt/testset/khan-academy/ > iwslt-khanacademy
+./scripts/find_files.sh ../../elitr-testset/documents/iwslt2020-nonnative-slt/testset/khan-academy/ > auto-iwslt2020-khanacademy
