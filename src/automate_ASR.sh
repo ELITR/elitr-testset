8ed7759b59104a71f2c2b7ae2e044b4bfc2826f7
pyRis <rishu_kumar@live.com>
2020-12-17 12:47:28 +0100

    minor updates

--- src/automate_ASR.sh
+++ src/automate_ASR.sh
@@ -6,6 +6,7 @@ if [ $# -ne 2 ] ; then
     exit
 fi
 
+# echo "This script is for en-EU fingerprints, update that manually if you are looking for other languages"
 
 echo "Cleaning the directory and copying new files"
 mkdir -p ../../../SLTev/SLTev-cache/OStt-tt-files/
@@ -41,8 +42,6 @@ done
 Processing ASR of files
 
 FROMASR=$2
-#Uncomment it after this run
-#FROM=$3
 index_name=$(basename $1)
 MEDIATOR=mediator.pervoice.com
 CLIENT="./ebclient -s $MEDIATOR -p 4448 --timestamps -r"
@@ -57,7 +56,10 @@ do
     mkdir -p $DIR
     mkdir -p $TRDIR
 	echo "$UNB $CLIENT -f $FROMASR -i en-EU -t text < $i | $TEETS $DIR/${file_name}.temp.asr" > $DIR/cmd
-	bash $DIR/cmd
+	nohup bash $DIR/cmd &
 
 	cat "$DIR/${file_name}.temp.asr" | cut -d" " -f2- | online-text-flow-events --text | sed -r '/^\s*$/d' > ${TRDIR}/${file_name}.en.asr
 done
+
+
+echo "Now, please run score_generation.sh for ASRev score."
