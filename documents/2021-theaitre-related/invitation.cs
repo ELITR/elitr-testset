d47c51cba6c07a46ad21ff2b0fa142bf68b7a486
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2021-02-09 10:41:09 +0100

    adding theaitre invitation

---
+++ documents/2021-theaitre-related/invitation.cs
@@ -0,0 +1,18 @@
+Švandovo divadlo na Smíchově a theAItre si Vás dovolují pozvat na on-line premiéru inscenace:
+AI: Když robot píše hru
+Režie: Daniel Hrbek
+Dramaturgie: Martina Kinská, David Košťák
+Scéna: Martin Šimek
+Kostýmy: Paulína Bočková
+Hudba: Daniel Hrbek
+Asistent režie: Blanka Popková,
+Vojtěch Nejedlý
+Produkce: Tereza Marková
+Hrají: Jacob Erftemeijer, Denisa Barešová, Andrea Buršová, Petr Buchta, Oskar Hes/Marek Frňka, Tomáš Petřík
+26. února 2021
+od 19 hodin
+on-line na www.theaitre.com
+Divadlo je scénou hl. m. Prahy a je podporováno městskou částí Praha 5
+Projekt je spolufinancován se státní podporou Technologické agentury ČR v rámci Programu ÉTA 3.
+Švandovo divadlo při příležitosti stoletého výročí premiéry hry R.U.R. Karla Čapka uvádí prezentaci unikátního projektu, který zkoumá, zda umělá inteligence dokáže napsat divadelní hru.
+Projekt probíhá ve spolupráci Švandova divadla, MFF UK a DAMU.
